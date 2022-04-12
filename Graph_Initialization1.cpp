6416128d954c4f357135d063fe53b948c4bb743a
Ram Gupta <86104680+RamGupta2000@users.noreply.github.com>
2022-04-12 12:22:27 +0530

    Add files via upload

---
+++ Graph_Initialization1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> adj[n + 1];
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    // vector<int> ans = adj[3];
+    // cout << ans[2];
+    return 0;
+}
