fe7ebffaa762c9e5793cd4d20a61de092268f79a
Ram Gupta <86104680+RamGupta2000@users.noreply.github.com>
2022-04-12 23:00:01 +0530

    Add files via upload

---
+++ Graph_TopologicalSort(BFS).cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> topoSort(int v, vector<int> adj[])
+    {
+        vector<int> ans;
+
+        vector<int> indegree(v, 0);
+        for (int i = 0; i < v; i++)
+            for (auto it : adj[i])
+                indegree[it]++;
+
+        queue<int> q;
+        for (int i = 0; i < v; i++)
+            if (indegree[i] == 0)
+                q.push(i);
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for (auto it : adj[node])
+            {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+        return ans;
+    }
+};
+
+int check(int V, vector<int> &res, vector<int> adj[])
+{
+
+    if (V != res.size())
+        return 0;
+
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++)
+    {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++)
+    {
+        for (int v : adj[i])
+        {
+            if (map[i] > map[v])
+                return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++)
+        {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        Solution obj;
+        vector<int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+
+    return 0;
+}
